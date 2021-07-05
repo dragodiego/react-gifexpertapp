9c3cfe07d54d8d983c34c512f56735f00c330d0c
Diego Bartolomé <diego.bartolome5@gmail.com>
2021-07-05 12:30:33 -0300

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+
+
+//TRAIGO LAS IMAGENES CON UNA PETICIÓN HTTP
+export const getGifs = async ( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=modsq3T4O2xZzpYkSstOhuy9RqMx9h46`;
+    const respuesta = await fetch( url );
+    const { data } = await respuesta.json();
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+    return gifs;
+
+}
